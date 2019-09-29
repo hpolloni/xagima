d77f6a853f69dbf3b72c65a05d99494405ec7c50
Herman Polloni <hpolloni@gmail.com>
2019-09-28 21:30:20 -0700

    Improving PCI probing. Improving array util class

--- src/include/utils/array.h
+++ src/include/utils/array.h
@@ -6,41 +6,42 @@
 template <typename T, size_t size = 256>
 class array {
 private:
-  mutable T data[size];
+  mutable T* data[size];
   mutable size_t len = 0;
+
 public:
   array() : len(0) {}
   
-  constexpr void add(const T& elem) noexcept {
-    data[len++] = elem;
+  constexpr void add(const T& elem) {
+    data[len++] = &(const_cast<T&>(elem));
   }
 
-  constexpr size_t length() noexcept {
+  constexpr size_t length() {
     return len;
   }
 
-  constexpr void add(const T& elem) const noexcept {
-    data[len++] = elem;
+  constexpr void add(const T& elem) const {
+    data[len++] = &elem;
   }
 
-  constexpr size_t length() const noexcept {
+  constexpr size_t length() const {
     return len;
   }
 
   constexpr T operator[] (int index) {
-    return data[index];
+    return *(data[index]);
   }
 
-  constexpr T operator[] (int index) const noexcept {
-    return data[index];
+  constexpr T operator[] (int index) const {
+    return *(data[index]);
   }
 
-  constexpr T* begin() const noexcept {
-    return &data[0];
+  constexpr T* begin() const {
+    return data[0];
   }
 
-  constexpr T* end() const noexcept {
-    return &data[len];
+  constexpr T* end() const {
+    return data[len];
   }
 };
 
